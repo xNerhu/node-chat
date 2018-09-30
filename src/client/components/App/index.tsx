7042046c31a05f0c4ed361652342f1768707fbfd
unknown <xnerhu@gmail.com>
2018-09-30 16:15:55 +0200

    Add basic form

--- src/client/components/App/index.tsx
+++ src/client/components/App/index.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  private input: HTMLInputElement;
+
+  private onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      this.send(this.input.value);
+    }
+  };
+
+  private onSendButtonClick = () => {
+    this.send(this.input.value);
+  };
+
+  public send = (message: string) => {
+    console.log(message);
+  };
+
   render() {
-    return <div>Hello world!</div>;
+    return (
+      <div className="app-container">
+        <div className="form">
+          <input
+            ref={r => (this.input = r)}
+            className="message-input"
+            type="text"
+            placeholder="Message"
+            onKeyPress={this.onKeyPress}
+          />
+          <div className="message-send" onClick={this.onSendButtonClick}>
+            SEND
+          </div>
+        </div>
+      </div>
+    );
   }
 }
